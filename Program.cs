90508eac2b12ce0c1592508067641c1df3a7ba9b
PSb19 <33777820+PSb19@users.noreply.github.com>
2023-09-27 15:24:08 +0200

    Program.cs update

--- Program.cs
+++ Program.cs
@@ -1,22 +1,17 @@
 using Microsoft.EntityFrameworkCore;
-using RESTfulAPI.Context;
 using RESTfulAPI.Controllers;
 using RESTfulAPI.Interfaces;
+using RESTfulAPI.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase("dbName"));
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ITaskResource, TaskResource>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -24,7 +19,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.MapControllers();
 
 app.Run();
